319064b8c38ec6d4609428e1d30ffb2c6ef92bcc
Vincent Hamp <vincent.hamp@higaski.at>
2024-04-19 10:44:14 +0200

    build: update to CMakeModules 0.8.0

--- examples/image_viewer/main.cpp
+++ examples/image_viewer/main.cpp
@@ -3,12 +3,6 @@
 #include "main_window.hpp"
 
 int main(int argc, char* argv[]) {
-  // Add executable path (+/plugins) to library path
-  auto const qstr_path{QString::fromStdString(
-    std::filesystem::path{argv[0uz]}.parent_path().string())};
-  QCoreApplication::addLibraryPath(qstr_path);
-  QCoreApplication::addLibraryPath(qstr_path + "/plugins");
-
   // Enable high DPI
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
